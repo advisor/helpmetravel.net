a04ff32efedc10fc5451411686646ca9e31ed4ff
advisor <projforge@gmail.com>
2013-11-09 19:27:20 +0300

    posts controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 HelpmetravelNet::Application.routes.draw do
-  get "welcome/index"
-  # The priority is based upon order of creation: first created -> highest priority.
+  # get "welcome/index"
+  # The priority is based upon orde r of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  resources :posts
   root 'welcome#index'
 
   # Example of regular route:
